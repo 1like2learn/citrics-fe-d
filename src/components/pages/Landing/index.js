9965b65c6ec16e6723fcf9aac00b879670659e08
1like2learn <ndespained@gmail.com>
2020-11-04 16:48:56 -0800

    adds styles

--- src/components/pages/Landing/index.js
+++ src/components/pages/Landing/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import logic from './logic.js';
+import logo from '../../../styles/images/logo.png';
+// import Header from '../../common/header';
+
+logic();
+
 export default () => (
-  <>
+  <div class="landingPage">
     <div class="headerComponent">Header Component</div>
-    <img alt="Citrics Logo"></img>
-    <h1>Citrics</h1>
-    <p>A Nomad's Guide to the City</p>
-  </>
+    <div>
+      <img src={logo} alt="Citrics Logo"></img>
+      <h1>Citrics</h1>
+      <p>A Nomad's Guide to the City</p>
+    </div>
+  </div>
 );
