c2dbde723a13fb031aee0e22f8be8181cc13a8ed
ianbautista <ianfbautista@gmail.com>
2020-11-05 09:50:38 -0800

    created MultipleCityCard component

---
+++ src/components/common/MultipleCityCard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function MultipleCityCard() {
+  return (
+    <div className="multiple-city-card-container">
+      <img src="#" alt="city image" />
+      <div>
+        <div>
+          <div className="city-card-color"></div>
+          <div className="city-card-cityName">{/* {cityName} */}</div>
+        </div>
+        <div className="city-card-cityDetails">{/* {cityDetails} */}</div>
+      </div>
+    </div>
+  );
+}
