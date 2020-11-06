7b11c6ec32f4b72bc80894594ba1cc33bf8666ad
1like2learn <ndespained@gmail.com>
2020-11-06 11:00:56 -0800

    Imports files

---
+++ src/components/common/DataPanel/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import logic from './logic.js';
+import dummyData from '../../../utils/dummyData';
+
+logic();
+
+export default function DataPanel() {
+  return <section></section>;
+}
