9c831d1738c05446bfa7ecbca7b8ea0a11a626d7
ianbautista <ianfbautista@gmail.com>
2020-11-09 12:04:22 -0800

    added chart and dummy data

--- src/components/common/MultipleCityCard/index.js
+++ src/components/common/MultipleCityCard/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import CloseButton from '../CloseButton';
 
 export default function MultipleCityCard(props) {
+  const { city } = props;
   return (
     <div className="multiple-city-card-container">
-      <img src="#" alt="city" />
+      <img src={city.wikiimgurl} alt={city.citynamestate} />
       <div>
         <div>
           <div className="city-card-color"></div>
           <div className="city-card-cityName">
             {CloseButton}
-            {props.cityName}
+            {city.citynamestate}
           </div>
         </div>
         <div className="city-card-cityDetails">{props.cityDetails}</div>
