272127307d06b02fa370befe9943828ecfeb9430
ianbautista <ianfbautista@gmail.com>
2020-11-09 15:08:57 -0800

    styled close button

--- src/components/common/CloseButton/index.js
+++ src/components/common/CloseButton/index.js
@@ -1,4 +1,17 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FaPlus } from 'react-icons/fa';
+
+const CloseButtonContainer = styled.button`
+  display: flex;
+  color: #d3553f;
+  transform: rotate(45deg);
+  background: none;
+  border: none;
+  outline: none;
+  font-size: 1.5rem;
+  align-self: center;
+`;
 
 export default function CloseButton() {
   const [isClose, setIsClosed] = useState(false);
@@ -7,5 +20,10 @@ export default function CloseButton() {
     e.preventDefault();
     setIsClosed(!isClose);
   };
-  return <button onClick={onClick}> x </button>;
+
+  return (
+    <CloseButtonContainer onClick={onClick}>
+      <FaPlus width="1.375rem" />
+    </CloseButtonContainer>
+  );
 }
