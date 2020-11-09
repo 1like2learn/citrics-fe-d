a04176c1734c222051a82b8002decf6a16a38d86
ianbautista <ianfbautista@gmail.com>
2020-11-09 15:13:08 -0800

    styled multiplecitycard

--- src/components/common/MultipleCityCard/index.js
+++ src/components/common/MultipleCityCard/index.js
@@ -1,21 +1,56 @@
 import React from 'react';
 import CloseButton from '../CloseButton';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  display: flex;
+  img {
+    width: 480px;
+    height: 450px;
+  }
+  .city-card-cityDetails-container {
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    border-bottom: 4px solid #2e3c48;
+    width: 100%;
+    .city-card-title-section {
+      display: flex;
+      justify-content: space-between;
+      width: inherit;
+      .city-card-cityName {
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-start;
+        font-size: 2.25rem;
+        font-family: 'Roboto';
+        font-weight: 700;
+      }
+    }
+    .city-card-cityDetails {
+      height: 100%;
+    }
+  }
+`;
 
 export default function MultipleCityCard(props) {
   const { city } = props;
   return (
-    <div className="multiple-city-card-container">
+    <CardContainer>
       <img src={city.wikiimgurl} alt={city.citynamestate} />
-      <div>
-        <div>
+      <div className="city-card-cityDetails-container">
+        <div className="city-card-title-section">
           <div className="city-card-color"></div>
           <div className="city-card-cityName">
-            {CloseButton}
+            <CloseButton />
             {city.citynamestate}
           </div>
         </div>
-        <div className="city-card-cityDetails">{props.cityDetails}</div>
+        <div className="city-card-cityDetails">
+          {' '}
+          {/*city details component goes here*/}
+        </div>
       </div>
-    </div>
+    </CardContainer>
   );
 }
