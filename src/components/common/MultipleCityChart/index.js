1d474c9cacf9f252c67fc720f95a46b39cc6e223
ianbautista <ianfbautista@gmail.com>
2020-11-10 14:35:24 -0800

    styled chart component

--- src/components/common/MultipleCityChart/index.js
+++ src/components/common/MultipleCityChart/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 
+import styled from 'styled-components';
+
+const ChartContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 360px;
+  margin: 0 auto;
+`;
+
 export default function MultipleCityChart(props) {
   const { temp_data } = props;
   const mydata = ''; // placeholder for real data
@@ -46,7 +57,7 @@ export default function MultipleCityChart(props) {
   };
 
   return (
-    <div className="multiple-city-chart-container">
+    <ChartContainer>
       <Line
         data={chartData}
         options={{
@@ -63,6 +74,6 @@ export default function MultipleCityChart(props) {
           },
         }}
       />
-    </div>
+    </ChartContainer>
   );
 }
