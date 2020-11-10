3878b2440cd32919fc3d9030aaa5bbe8824e5ced
1like2learn <ndespained@gmail.com>
2020-11-10 14:33:52 -0800

    separates styles from html file

--- src/components/pages/Landing/index.js
+++ src/components/pages/Landing/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 import logic from './logic.js';
 import logo from './logo.png';
+import heroImage from './heroImage.png';
+import LandingPageDiv from './style';
 import { LandingHeader } from '../../common/Header';
 
 logic();
 
 export default function LandingPage() {
   return (
-    <div className="landingPage">
+    <LandingPageDiv style={{ backgroundImage: `url(${heroImage})` }}>
       <LandingHeader />
       <div className="landingPageCont">
         <img src={logo} alt="Citrics Logo"></img>
         <h1>Citrics</h1>
         <p>A Nomad's Guide to the City</p>
       </div>
-    </div>
+    </LandingPageDiv>
   );
 }
