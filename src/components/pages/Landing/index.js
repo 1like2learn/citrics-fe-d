78630c2f15a492e9eb047e34a7ecf3c1006eafb3
1like2learn <ndespained@gmail.com>
2020-11-10 13:12:39 -0800

    Integrates LandingHeader component

--- src/components/pages/Landing/index.js
+++ src/components/pages/Landing/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import logic from './logic.js';
 import logo from './logo.png';
-// import Header from '../../common/header';
+import { LandingHeader } from '../../common/Header';
 
 logic();
 
 export default function LandingPage() {
   return (
     <div className="landingPage">
-      <div className="headerComponent">Header Component</div>
-      <div>
+      <LandingHeader />
+      <div className="landingPageCont">
         <img src={logo} alt="Citrics Logo"></img>
         <h1>Citrics</h1>
         <p>A Nomad's Guide to the City</p>
