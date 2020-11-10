faf0d192b0efed9d5b64558c63d996fb90eb7b86
1like2learn <ndespained@gmail.com>
2020-11-10 13:21:31 -0800

    Fixes a bit of formating

--- src/index.js
+++ src/index.js
@@ -48,8 +48,9 @@ function App() {
       <Route path="/compare" component={ComparisonPage} />
       <Route path="/" exact component={LandingPage} />
       <Route path="/multiple-city" component={MultipleCityPage} />
+      <Route path="/city" component={CitySearch} />
 
-      {/* temporairly putting the landing component until we have a profile/favorites page */}
+      {/* temporarily putting the landing component until we have a profile/favorites page */}
       <Security {...config} onAuthRequired={authHandler}>
         <SecureRoute path="/profile" component={LandingPage} />
         <Route path="/implicit/callback" component={LoginCallback} />
