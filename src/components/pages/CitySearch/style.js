fca3b4ee6ea70dbd4f280064dceee055e307756c
ianbautista <ianfbautista@gmail.com>
2020-11-11 17:53:34 -0800

    styled city search header

--- src/components/pages/CitySearch/style.js
+++ src/components/pages/CitySearch/style.js
@@ -47,6 +47,8 @@ const CitySearchDiv = styled.div`
         div.citySearchSingleCityChart {
           height: 25vh;
         }
+        .city-search-details {
+        }
       }
     }
   }
@@ -55,68 +57,90 @@ const CitySearchDiv = styled.div`
 const HeaderBar = styled.div`
   position: absolute;
   display: flex;
+  flex-direction: row;
   width: 100%;
   height: 15vh;
   align-items: center;
   align-content: center;
   margin: 0 auto;
-  background-color: rgba(00, 00, 00, 0.7);
-
-  .search {
-    text-align: center;
+  background-color: rgba(00, 00, 00, 0);
+  z-index: 100;
+  .left {
     display: flex;
     flex-direction: row;
-    width: 60%;
-    align-items: center;
-    align-content: center;
-  }
+    justify-content: center;
+    width: 50%;
 
-  .aside {
-    display: flex;
-    font-size: 0.6rem;
-    align-items: flex-end;
-    color: #ffffff;
+    .search {
+      display: flex;
+      flex-direction: column;
+      width: 60%;
+      align-items: flex-end;
+      align-content: center;
+      justify-content: center;
+      padding: 0;
+    }
+
+    .aside {
+      display: flex;
+      font-size: 0.6rem;
+      align-items: flex-end;
+      color: #ffffff;
+      margin: 0;
+    }
   }
 
   .right {
     display: flex;
-    justify-content: center;
-    align-content: center;
+    justify-content: flex-end;
+    align-content: flex-end;
     font-size: 1rem;
+    color: #5e5e5e;
     flex-direction: row;
-    width: 35%;
-    margin: 1% auto;
-
-    .login {
-      background-color: rgba(00, 00, 00, 0);
-    }
-
-    button {
-      margin: 2%;
-      width: 100%;
-      background-color: #d35540;
-      border: 0;
-      border-radius: 25px;
-      color: #ffffff;
-      height: 20px;
-      font-family: 'Norwester', sans serif;
-    }
+    width: 50%;
+    padding: 1% 2.5%;
   }
 `;
 const SearchBar = styled.form`
-  margin: 0 5%;
-  width: 60%;
-
-  input {
-    position: relative;
+  display: flex;
+  margin: auto 0;
+  width: 370px;
+  height: 46.16px;
+  .search-icon-container {
     display: flex;
-    border-radius: 10px;
-    border: 0;
-    display: inline-block;
-    text-align: center;
-    margin: 0 2%;
-    height: 20px;
+    align-items: center;
+    background-color: #ffffff;
+    border-radius: 0 15px 15px 0;
+    color: #c4c4c4;
+    height: 46.16px;
+    width: 55px;
+    padding: 0 4%;
+    .search-icon {
+      height: 27.56px;
+      width: 27.56px;
+      color: #c4c4c4;
+    }
+  }
+  label {
     width: 100%;
+    input {
+      display: flex;
+      border-radius: 15px 0 0 15px;
+      border: 0;
+      display: inline-block;
+      text-align: start;
+      padding: 0 5%;
+      height: 46.16px;
+      width: 100%;
+      &::placeholder {
+        color: #c4c4c4;
+        font-size: 1.5rem;
+      }
+      &:focus {
+        border: none;
+        outline: none;
+      }
+    }
   }
 `;
 
