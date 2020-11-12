24f363f8b91284265612337503bde6230e039ce2
ianbautista <ianfbautista@gmail.com>
2020-11-12 13:37:38 -0800

    fixes styling for landingpage header

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { FaSearchengin } from 'react-icons/fa';
+import { FaSearchengin, FaSearch } from 'react-icons/fa';
 
 const HeaderBar = styled.div`
   position: relative;
@@ -10,12 +10,12 @@ const HeaderBar = styled.div`
   align-items: center;
   align-content: center;
   margin: 0 auto;
-  background-color: rgba(00, 00, 00, 0.7);
+  background-color: rgba(00, 00, 00, 0.3);
 
   .search {
-    text-align: center;
     display: flex;
     flex-direction: row;
+    text-align: center;
     width: 60%;
     align-items: center;
     align-content: center;
@@ -23,51 +23,103 @@ const HeaderBar = styled.div`
 
   .aside {
     display: flex;
-    font-size: 0.6rem;
-    align-items: flex-end;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 15%;
+    font-size: 0.875rem;
     color: #ffffff;
+    cursor: pointer;
+    .advanced-search {
+      font-family: 'Roboto', sans-serif;
+      text-align: left;
+      line-height: 1.1rem;
+      margin-right: 1%;
+      margin: auto 1%;
+    }
+    .advanced-search-icon {
+      width: 40px;
+      height: 40px;
+    }
   }
 
   .right {
     display: flex;
-    justify-content: center;
+    justify-content: flex-end;
     align-content: center;
-    font-size: 1rem;
     flex-direction: row;
     width: 35%;
-    margin: 1% auto;
+    margin-right: 5%;
 
-    .login {
-      background-color: rgba(00, 00, 00, 0);
+    .login-btn {
+      background-color: unset;
+      border: unset;
+      font-size: 1.5rem;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 700;
+      color: #ffffff;
+      cursor: pointer;
     }
 
-    button {
+    .compare-cities-btn {
       margin: 2%;
-      width: 100%;
+      width: 13.3125rem;
       background-color: #d35540;
       border: 0;
-      border-radius: 25px;
+      border-radius: 15px;
       color: #ffffff;
-      height: 20px;
-      font-family: 'Norwester', sans serif;
+      height: 3rem;
+      font-size: 1.5rem;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 700;
+      cursor: pointer;
     }
   }
 `;
 
 const SearchBar = styled.form`
-  margin: 0 5%;
-  width: 60%;
-
-  input {
-    position: relative;
+  display: flex;
+  margin: auto 0;
+  margin-left: 10%;
+  width: 370px;
+  height: 46.16px;
+  .search-icon-container {
     display: flex;
-    border-radius: 10px;
-    border: 0;
-    display: inline-block;
-    text-align: center;
-    margin: 0 2%;
-    height: 20px;
+    align-items: center;
+    background-color: #ffffff;
+    border-radius: 0 15px 15px 0;
+    color: #c4c4c4;
+    height: 46.16px;
+    width: 55px;
+    padding: 0 4%;
+    .search-icon {
+      height: 27.56px;
+      width: 27.56px;
+      color: #c4c4c4;
+    }
+  }
+  label {
     width: 100%;
+    input {
+      display: flex;
+      border-radius: 15px 0 0 15px;
+      border: 0;
+      display: inline-block;
+      text-align: start;
+      padding: 0 5%;
+      height: 46.16px;
+      width: 100%;
+      &::placeholder {
+        color: #c4c4c4;
+        font-size: 1.5rem;
+        font-family: 'Roboto', sans-serif;
+      }
+      &:focus {
+        border: none;
+        outline: none;
+      }
+    }
   }
 `;
 
@@ -89,24 +141,26 @@ export const LandingHeader = () => {
                 name="term"
                 value={searchTerm.term}
                 onChange={handleChanges}
-                placeholder="Start your search..."
+                placeholder="search"
                 type="text"
               />
             </label>
+            <div className="search-icon-container">
+              <FaSearch className="search-icon" />
+            </div>
           </SearchBar>
 
           <div className="aside">
-            <p>
+            <p className="advanced-search">
               Advanced <br /> Search
-            </p>{' '}
-            &nbsp;
-            <FaSearchengin size="25px" />
+            </p>
+            <FaSearchengin size="40px" className="advanced-search-icon" />
           </div>
         </div>
 
         <div className="right">
-          <button className="login">Login</button>
-          <button>Compare Cities</button>
+          <button className="login-btn">Login</button>
+          <button className="compare-cities-btn">Compare Cities</button>
         </div>
       </HeaderBar>
     </div>
