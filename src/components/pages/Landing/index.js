67676bbca05e369153f5d2b3cbec706bc28e7492
ianbautista <ianfbautista@gmail.com>
2020-11-12 13:36:28 -0800

    fixes styling in landing page

--- src/components/pages/Landing/index.js
+++ src/components/pages/Landing/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import logic from './logic.js';
-import logo from './logo.png';
+import logo from '../../../assets/citricsIcon.svg';
 import heroImage from './heroImage.png';
 import LandingPageDiv from './style';
 import { LandingHeader } from '../../common/Header';
@@ -13,9 +13,9 @@ export default function LandingPage() {
     <LandingPageDiv style={{ backgroundImage: `url(${heroImage})` }}>
       <LandingHeader />
       <div className="landingPageCont">
-        <img src={logo} alt="Citrics Logo"></img>
-        <h1>Citrics</h1>
-        <p>A Nomad's Guide to the City</p>
+        <img src={logo} alt="Citrics Logo" className="landing-page-logo"></img>
+        <h1 className="landing-page-h1">CITRICS</h1>
+        <p className="landing-page-p">A NOMAD'S GUIDE TO THE CITY </p>
       </div>
     </LandingPageDiv>
   );
