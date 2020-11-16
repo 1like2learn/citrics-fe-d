71ec082ee005e6146afa8498bf8077cb7a782a6e
ianbautista <ianfbautista@gmail.com>
2020-11-16 11:25:48 -0800

    deletes unused files

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,14 +2,16 @@ import { ComparisonPage } from './ComparisonPage';
 import LandingPage from './Landing';
 import { LoginPage } from './Login';
 import { NotFoundPage } from './NotFound';
-import FavoritesPage from './Favorites';
 import CitySearch from './CitySearch';
+import MultipleCityPage from './MultipleCityPage';
+import { ProfilePage } from './ProfilePage';
 // test
 export {
   ComparisonPage,
   LandingPage,
   LoginPage,
   NotFoundPage,
-  FavoritesPage,
   CitySearch,
+  MultipleCityPage,
+  ProfilePage,
 };
