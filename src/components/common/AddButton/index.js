fd5e1f70af10331a5b220b6a3b1a1f47b9023db8
ianbautista <ianfbautista@gmail.com>
2020-11-18 09:43:22 -0800

    fixes styling

--- src/components/common/AddButton/index.js
+++ src/components/common/AddButton/index.js
@@ -3,20 +3,25 @@ import { FaPlus } from 'react-icons/fa';
 import styled from 'styled-components';
 
 const AddButtonContainer = styled.div`
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #d3553f;
   border-radius: 20px;
+  width: 50px;
+  height: 50px;
   .add-button-icon {
     color: #fff;
+    width: 22px;
+    height: 22px;
   }
 `;
 
 export default function AddButton() {
   return (
     <AddButtonContainer>
-      <FaPlus size="50px" className="add-button-icon" />
+      <FaPlus size="22px" className="add-button-icon" />
     </AddButtonContainer>
   );
 }
