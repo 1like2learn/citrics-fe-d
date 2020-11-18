4eca45bdd966390af381b9a6fc63642d0e210f25
1like2learn <ndespained@gmail.com>
2020-11-18 07:29:49 -0800

    deletes unused state files

--- src/state/actions/index.js
+++
@@ -1,6 +0,0 @@
-import * as searchBarActs from './searchBarActs';
-import * as cityCardActs from './cityCardActs';
-import * as cardContainerActs from './cardContainerActs';
-import * as userActs from './userActs';
-
-export { searchBarActs, cardContainerActs, cityCardActs, userActs };
