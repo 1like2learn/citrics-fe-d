782adc249e47f69a879bb397d040b0c1a8cb042b
1like2learn <ndespained@gmail.com>
2020-11-18 08:28:46 -0800

    removes unused code in comparison page

--- src/components/pages/ComparisonPage/index.js
+++ src/components/pages/ComparisonPage/index.js
@@ -1,17 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { fetchCities } from '../../../state/actions/searchBarActs';
+import React from 'react';
 import Container from './Container';
 
 export default function ComparisonPage(props) {
   const { currentCities } = props;
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const cities = useSelector(state => state.cityData);
-  const searching = useSelector(state => state.isSearching);
-  const cityOne = useSelector(state => state.cityOne);
-  const cityTwo = useSelector(state => state.cityTwo);
 
   return (
     <>
